e0a8f020072cd80ea48e313a4ccbe0f9728c656e
Manara Ali <manaraali64@gmail.com>
2021-09-24 12:54:41 -0400

    Add logic to navbar links and styling

--- jsx/src/components/App.jsx
+++ jsx/src/components/App.jsx
@@ -1,13 +1,44 @@
+// IMPORT APP CSS
+import "./App.css";
+
 // 1. IMPORT REACT
 import React from "react";
 
 // 2. ADDITIONAL IMPORTS
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./navbar";
+import Home from "./home";
+import Services from "./services";
+import Contact from "./contact";
+import Login from "./login";
+import SignUp from "./signup";
+import Footer from "./footer";
 
 // 3. CREATE A FUNCTION BASED COMPONENT
 const App = () => {
   return (
     <div>
-      <h1>App Component</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 };
