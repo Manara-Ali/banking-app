5e43a576cd813dc5f5158f4234d485f2bfcb353f
Manara Ali <manaraali64@gmail.com>
2021-09-25 05:09:30 -0400

    Use window location to properly position the tab marker

--- jsx/src/components/App.jsx
+++ jsx/src/components/App.jsx
@@ -2,7 +2,7 @@
 import "./App.css";
 
 // 1. IMPORT REACT
-import React from "react";
+import React, { useEffect } from "react";
 
 // 2. ADDITIONAL IMPORTS
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -16,6 +16,20 @@ import Footer from "./footer";
 
 // 3. CREATE A FUNCTION BASED COMPONENT
 const App = () => {
+  // Use this Effect to keep the tab orange indicator where it currently is whenever the page is refreshed
+  useEffect(() => {
+    // Find the current location of the tab indicator using the url location
+    const currentElement = window.location.pathname.split("/")[1];
+
+    // If there is a path following the home url, remove the indicator from the home tab (which is the default when reloading the page) and add it to the current url location
+    if (currentElement) {
+      document.querySelector(".current").classList.remove("current");
+      document.querySelector(`#${currentElement}`).classList.add("current");
+    } else {
+      // Otherwise if the tab is already home the split method will return null, therefore we need to manually add it to the home tab
+      document.querySelector("#home").classList.add("current");
+    }
+  }, []);
   return (
     <div>
       <Router>
