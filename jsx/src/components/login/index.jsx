d0284ef4a056f953118694a3931d14fbca691198
Manara Ali <manaraali64@gmail.com>
2021-09-25 05:28:18 -0400

    Create a login form

--- jsx/src/components/login/index.jsx
+++ jsx/src/components/login/index.jsx
@@ -5,9 +5,36 @@ import React from "react";
 
 // 3. CREATE A FUNCTION BASED COMPONENT
 const Login = () => {
+  // Handles form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div id="container">
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="form-wrap">
+        <h1>Log In</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pin">PIN</label>
+            <input type="text" name="pin" id="pin" />
+          </div>
+          <button
+            onClick={(e) => {
+              return handleSubmit(e);
+            }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
